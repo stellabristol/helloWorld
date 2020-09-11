8d59fdfc556405430847cfc6095c89fe35e68905
Stella Bristol <stellabristol@Stellas-MacBook-Air.local>
2020-09-11 10:43:12 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+
+Project1: hello world
+
+Description: Print hello world!
+
+Date: 09/11/2020
+
+Class: CS117
+
+Author: Stella Bristol
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello world!" << endl;
+
+    return 0;
+}
